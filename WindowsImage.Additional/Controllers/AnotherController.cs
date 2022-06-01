34f7c63e2b912511e8b3abc2964aa4a91b9bc980
Jim Paine <japaine@microsoft.com>
2022-06-01 13:53:42 +0100

    first

---
+++ WindowsImage.Additional/Controllers/AnotherController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WindowsImage.Additional.Controllers
+{
+    public class AnotherController : Controller
+    {
+        public ActionResult Index()
+        {
+            var v = View();
+            return v;
+        }
+    }
+}
---
+++ WindowsImage/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using WindowsImage.Additional.Controllers;
+using WindowsImage.Controllers;
+
+namespace WindowsImage
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                namespaces: new[] { typeof(HomeController).Namespace, typeof(AnotherController).Namespace },
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
